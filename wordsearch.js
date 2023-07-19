a6bbe781b1906f2970620e5f77045aa03949baf5
luwam <lwamyohannes2018@gmail.com>
2023-07-18 23:07:29 -0600

    updated

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// pair programming by luwam beyin and taylor maine
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let wordFound = false;
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  return wordFound;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
